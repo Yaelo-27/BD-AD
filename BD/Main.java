89af49b60adf67bc12ef2d6872c0ccdb0d26c16e
Yaelo-27 <78753436+Yaelo-27@users.noreply.github.com>
2021-11-17 17:14:54 +0100

    Add files via upload

--- BD/Main.java
+++ BD/Main.java
@@ -12,8 +12,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Métodos para hacer consultas descomentar uno otro para que funcione, eliminar los registros de la db para que no salte error de duplicado
-		constatement();
-		//conpreparedstatement();
+		//constatement();
+		conpreparedstatement();
 	}
 	
 	//Ejecucion con statement
@@ -22,6 +22,7 @@ public class Main {
 		
 		try {
 			//General
+			System.out.println("Método con statement()");
 			BufferedWriter wr = new BufferedWriter(new FileWriter(new File("datosdb.txt")));
 			Connection conexion=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/nba","root","admin");
 			DatabaseMetaData datos = conexion.getMetaData();
@@ -56,8 +57,10 @@ public class Main {
 			datosnba = "select * from equipos";
 			
 			rs = s.executeQuery(datosnba);
+			System.out.println("Apartado 4: insertar datos"+"\n");
 			aux.write("Apartado 4: insertar datos"+"\n");
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			aux.newLine();
@@ -66,8 +69,10 @@ public class Main {
 			s.executeUpdate(el);
 			
 			rs = s.executeQuery(datosnba);
+			System.out.println("Apartado 4: Eliminar equipo black"+"\n");
 			aux.write("Apartado 4: Eliminar equipo black"+"\n");
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			aux.newLine();
@@ -78,17 +83,21 @@ public class Main {
 			s.executeUpdate(upda);
 			
 			rs = s.executeQuery(datosnba);
+			System.out.println("Apartado 4:  Modificar equipo bulls conferencia"+"\n");
 			aux.write("Apartado 4: Modificar equipo bulls conferencia"+"\n");
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			
 			aux.newLine();
 			String datos_inversos = "SELECT * FROM equipos ORDER BY Nombre DESC";
+			System.out.println("Apartado 4:   orden inverso"+"\n");
 			aux.write("Apartado 4: orden inverso"+"\n");
 			rs = s.executeQuery(datos_inversos);
 			
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			
@@ -107,6 +116,7 @@ public class Main {
 		
 		try {
 			//General
+			System.out.println("Con PreparedStatement()");
 			BufferedWriter wr = new BufferedWriter(new FileWriter(new File("datosdbpreparedstatement.txt")));
 			Connection conexion=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/nba","root","admin");
 			DatabaseMetaData datos = conexion.getMetaData();
@@ -127,8 +137,9 @@ public class Main {
 			System.out.println("Ejecucion terminada: Fichero datosdbpreparedstatement.txt escrito");		
 			wr.close();
 			
+		
 			//Apartado 4
-			BufferedWriter aux = new BufferedWriter(new FileWriter(new File("crudpreparedstatement.txt")));
+			BufferedWriter aux = new BufferedWriter(new FileWriter(new File("crud.txt")));
 			
 			//Insertar datos
 			String ins1 = "insert into equipos(Nombre,Ciudad,Conferencia,Division) values('Blackbulls','Madrid','SP','2DIV')";
@@ -139,9 +150,12 @@ public class Main {
 			s.executeUpdate(ins3);
 			
 			datosnba = "select * from equipos";
+			
 			rs = s.executeQuery(datosnba);
+			System.out.println("Apartado 4: insertar datos"+"\n");
 			aux.write("Apartado 4: insertar datos"+"\n");
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			aux.newLine();
@@ -150,8 +164,10 @@ public class Main {
 			s.executeUpdate(el);
 			
 			rs = s.executeQuery(datosnba);
+			System.out.println("Apartado 4: Eliminar equipo black"+"\n");
 			aux.write("Apartado 4: Eliminar equipo black"+"\n");
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			aux.newLine();
@@ -162,24 +178,28 @@ public class Main {
 			s.executeUpdate(upda);
 			
 			rs = s.executeQuery(datosnba);
+			System.out.println("Apartado 4:  Modificar equipo bulls conferencia"+"\n");
 			aux.write("Apartado 4: Modificar equipo bulls conferencia"+"\n");
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			
 			aux.newLine();
 			String datos_inversos = "SELECT * FROM equipos ORDER BY Nombre DESC";
+			System.out.println("Apartado 4:   orden inverso"+"\n");
 			aux.write("Apartado 4: orden inverso"+"\n");
 			rs = s.executeQuery(datos_inversos);
 			
 			while(rs.next()) {
+				System.out.println("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 				aux.write("Nombre de equipo -->"+rs.getString(1)+" Ciudad -->"+rs.getString(2)+" Conferencia-->"+rs.getString(3)+" Division-->"+rs.getString(4)+"\n");
 			}
 			
 			
 			aux.close();
 			
-			System.out.println("Ejecucion terminada crudpreparedstatement.txt escrito correctamente!");
+			System.out.println("Ejecucion terminada crud.txt escrito correctamente!");
 		}catch(SQLException |IOException ex) {
 			ex.printStackTrace();
 		}
